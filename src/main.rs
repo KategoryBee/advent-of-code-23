9c5f13c27411b0a06a681d0b2f2b322e5819e8a1
Katherine Broadbent <kategory.bee@gmail.com>
2023-12-02 07:54:30 +1000

    Add test runs, clean up in prep for day 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
 
 fn main() {
-    let input = read_lines("input.txt").unwrap();
+    let test_result = process("test.txt");
+    assert_eq!(test_result, 281, "test input failed");
+
+    let result = process("input.txt");
+    println!("result: {result}");
+}
+
+fn process(path: &str) -> i32 {
+    let input = read_lines(path).unwrap();
     let mut sum = 0;
     for line in input {
         let (first, last) = first_last_digit(&line.unwrap());
@@ -10,11 +18,11 @@ fn main() {
         sum += this_line;
     }
 
-    println!("Result: {sum}");
+    sum
 }
 
 fn first_last_digit(mut input: &str) -> (i32, i32) {
-    const TESTS: [(i32, &str, &str); 10] = [
+    static INPUT_CHECKS: [(i32, &str, &str); 10] = [
         (0, "0", "zero"),
         (1, "1", "one"),
         (2, "2", "two"),
@@ -31,12 +39,10 @@ fn first_last_digit(mut input: &str) -> (i32, i32) {
     let mut last = None;
 
     while !input.is_empty() {
-        for (val, dig_test, str_test) in TESTS {
-            if input.starts_with(dig_test) || input.starts_with(str_test) {
+        for (val, digit_test, str_test) in INPUT_CHECKS {
+            if input.starts_with(digit_test) || input.starts_with(str_test) {
                 last = Some(val);
-                if first.is_none() {
-                    first = last;
-                }
+                first = first.or(last);
             }
         }
 
@@ -46,10 +52,7 @@ fn first_last_digit(mut input: &str) -> (i32, i32) {
     (first.unwrap(), last.unwrap())
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<std::fs::File>>>
-where
-    P: AsRef<std::path::Path>,
-{
+fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<std::fs::File>>> {
     let file = std::fs::File::open(filename)?;
     Ok(io::BufRead::lines(io::BufReader::new(file)))
 }
