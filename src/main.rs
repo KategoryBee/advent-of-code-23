7114f2743e58aa2ae65011f73af04ace666a3374
Katherine Broadbent <kategory.bee@gmail.com>
2023-12-02 15:17:56 +1000

    Day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,10 @@ use std::io;
 
 fn main() {
     let test_result = solve("test.txt");
-    assert_eq!(test_result, 8, "test input failed");
+    assert_eq!(test_result, 2286, "test input failed");
 
     let result = solve("input.txt");
     println!("result: {result}");
-}
-
-fn limit_for_colour(colour: &str) -> Option<i32> {
-    static LIMITS: [(&str, i32); 3] = [("red", 12), ("green", 13), ("blue", 14)];
-
-    for l in LIMITS {
-        if l.0 == colour {
-            return Some(l.1);
-        }
-    }
-
-    None
 }
 
 fn solve(path: &str) -> i32 {
@@ -27,27 +15,32 @@ fn solve(path: &str) -> i32 {
 
     for line in input {
         let line = line.unwrap();
-        let (game_id, reveals) = line.split_once(':').unwrap();
+        let (_game_id, reveals) = line.split_once(':').unwrap();
 
-        let game_id: i32 = game_id.strip_prefix("Game ").unwrap().parse().unwrap();
-
-        let mut all_ok = true;
+        let mut most_red = 0;
+        let mut most_green = 0;
+        let mut most_blue = 0;
 
         for reveal in reveals.split(';') {
-            for color in reveal.split(',') {
-                let (amount, color) = color.trim().split_once(' ').unwrap();
+            for colour in reveal.split(',') {
+                let (amount, colour) = colour.trim().split_once(' ').unwrap();
                 let amount: i32 = amount.parse().unwrap();
 
-                let limit = limit_for_colour(color).unwrap();
-                if amount > limit {
-                    all_ok = false;
+                let bucket = match colour {
+                    "red" => &mut most_red,
+                    "green" => &mut most_green,
+                    "blue" => &mut most_blue,
+                    _ => panic!("unknown colour{}", colour),
+                };
+
+                if *bucket < amount {
+                    *bucket = amount;
                 }
             }
         }
 
-        if all_ok {
-            solution += game_id;
-        }
+        let cube_power = most_red * most_green * most_blue;
+        solution += cube_power;
     }
 
     solution
