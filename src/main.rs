8fd08737b1e4b875299a5702f435cf4ab5ce0c55
Katherine Broadbent <kategory.bee@gmail.com>
2023-12-05 13:53:45 +1000

    day 4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,44 @@
-use std::{collections::HashMap, io};
-
-use regex::bytes::Regex;
+use std::{collections::HashSet, io};
 
 fn main() {
     let test_result = solve("test.txt");
-    assert_eq!(test_result, 467835, "test input failed");
+    assert_eq!(test_result, 13, "test input failed");
 
     let result = solve("input.txt");
     println!("result: {result}");
 }
 
-fn is_gear(b: u8) -> bool {
-    b == b'*'
-}
-
-struct Gear {
-    ratio: i32,
-    connected_count: i32,
-}
-
-impl Default for Gear {
-    fn default() -> Self {
-        Self {
-            ratio: 1,
-            connected_count: 0,
-        }
-    }
-}
-
-fn gear_locations(path: &str) -> HashMap<(i32, i32), Gear> {
-    let mut res = HashMap::new();
-
-    let input = read_lines(path).unwrap();
-
-    for (row, line) in input.enumerate() {
-        let line = line.unwrap();
-        for (col, character) in line.bytes().enumerate() {
-            if is_gear(character) {
-                res.insert((row as i32, col as i32), Gear::default());
-            }
-        }
-    }
-
-    res
-}
-
 fn solve(path: &str) -> i32 {
-    let next_number = Regex::new(r"\d+").unwrap();
-
-    let mut gears = gear_locations(path);
-
     let input = read_lines(path).unwrap();
-
-    for (row, line) in input.enumerate() {
-        let line = line.unwrap();
-        let line = line.as_bytes();
-
-        let mut offset = 0;
-        loop {
-            let Some(caps) = next_number.captures_at(line, offset) else {
-                // no more numbers in line
-                break;
-            };
-
-            let digits = caps.get(0).unwrap();
-
-            let digits_str = std::str::from_utf8(digits.as_bytes()).unwrap();
-            let digits_val: i32 = digits_str.parse().unwrap();
-
-            for r in (row as i32 - 1)..=(row as i32 + 1) {
-                for c in (digits.start() as i32 - 1)..=(digits.end() as i32) {
-                    if let Some(gear) = gears.get_mut(&(r, c)) {
-                        gear.connected_count += 1;
-                        gear.ratio *= digits_val;
-                    }
-                }
-            }
-
-            offset = digits.end();
-        }
-    }
 
     let mut solution = 0;
-    for (_loc, gear) in gears {
-        if gear.connected_count > 1 {
-            solution += gear.ratio;
+
+    let mut winning_numbers: HashSet<i32> = HashSet::with_capacity(20);
+
+    for line in input {
+        winning_numbers.clear();
+
+        let line = line.unwrap();
+        let after_card = line.split_once(':').unwrap().1;
+        let (winning, have) = after_card.split_once('|').unwrap();
+
+        for w in winning.split_whitespace() {
+            winning_numbers.insert(w.parse().unwrap());
         }
+
+        let mut points = 0;
+        for a in have.split_whitespace() {
+            let num: i32 = a.parse().unwrap();
+            if winning_numbers.contains(&num) {
+                if points == 0 {
+                    points = 1;
+                } else {
+                    points *= 2;
+                }
+            }
+        }
+
+        solution += points;
     }
 
     solution
