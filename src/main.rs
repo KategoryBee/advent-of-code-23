3420ce6ddc341b86347933969fbc7edb4c4250dd
Katherine Broadbent <kategory.bee@gmail.com>
2023-12-05 13:32:05 +1000

    day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
-use std::{collections::HashSet, io};
+use std::{collections::HashMap, io};
 
 use regex::bytes::Regex;
 
 fn main() {
     let test_result = solve("test.txt");
-    assert_eq!(test_result, 4361, "test input failed");
+    assert_eq!(test_result, 467835, "test input failed");
 
     let result = solve("input.txt");
     println!("result: {result}");
 }
 
-fn is_symbol(b: u8) -> bool {
-    !(b.is_ascii_digit() || b == b'.')
+fn is_gear(b: u8) -> bool {
+    b == b'*'
 }
 
-fn symbol_locations(path: &str) -> HashSet<(i32, i32)> {
-    let mut res = HashSet::new();
+struct Gear {
+    ratio: i32,
+    connected_count: i32,
+}
+
+impl Default for Gear {
+    fn default() -> Self {
+        Self {
+            ratio: 1,
+            connected_count: 0,
+        }
+    }
+}
+
+fn gear_locations(path: &str) -> HashMap<(i32, i32), Gear> {
+    let mut res = HashMap::new();
 
     let input = read_lines(path).unwrap();
 
     for (row, line) in input.enumerate() {
         let line = line.unwrap();
         for (col, character) in line.bytes().enumerate() {
-            if is_symbol(character) {
-                res.insert((row as i32, col as i32));
+            if is_gear(character) {
+                res.insert((row as i32, col as i32), Gear::default());
             }
         }
     }
@@ -32,11 +46,9 @@ fn symbol_locations(path: &str) -> HashSet<(i32, i32)> {
 }
 
 fn solve(path: &str) -> i32 {
-    let mut solution = 0;
-
     let next_number = Regex::new(r"\d+").unwrap();
 
-    let locations = symbol_locations(path);
+    let mut gears = gear_locations(path);
 
     let input = read_lines(path).unwrap();
 
@@ -53,22 +65,26 @@ fn solve(path: &str) -> i32 {
 
             let digits = caps.get(0).unwrap();
 
-            let mut has_symbol = false;
+            let digits_str = std::str::from_utf8(digits.as_bytes()).unwrap();
+            let digits_val: i32 = digits_str.parse().unwrap();
+
             for r in (row as i32 - 1)..=(row as i32 + 1) {
                 for c in (digits.start() as i32 - 1)..=(digits.end() as i32) {
-                    if locations.contains(&(r, c)) {
-                        has_symbol = true;
+                    if let Some(gear) = gears.get_mut(&(r, c)) {
+                        gear.connected_count += 1;
+                        gear.ratio *= digits_val;
                     }
                 }
             }
 
-            if has_symbol {
-                let digits_str = std::str::from_utf8(digits.as_bytes()).unwrap();
-                let digits_val: i32 = digits_str.parse().unwrap();
-                solution += digits_val;
-            }
-
             offset = digits.end();
+        }
+    }
+
+    let mut solution = 0;
+    for (_loc, gear) in gears {
+        if gear.connected_count > 1 {
+            solution += gear.ratio;
         }
     }
 
