f5fd763e27da661da70e6918a8f8a5650cd71cde
Katherine Broadbent <kategory.bee@gmail.com>
2023-12-06 07:49:02 +1000

    day 4 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{collections::HashSet, io};
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 
 fn main() {
     let test_result = solve("test.txt");
-    assert_eq!(test_result, 13, "test input failed");
+    assert_eq!(test_result, 30, "test input failed");
 
     let result = solve("input.txt");
     println!("result: {result}");
@@ -11,12 +14,15 @@ fn main() {
 fn solve(path: &str) -> i32 {
     let input = read_lines(path).unwrap();
 
-    let mut solution = 0;
-
+    let mut cards_count: HashMap<usize, i32> = HashMap::new();
     let mut winning_numbers: HashSet<i32> = HashSet::with_capacity(20);
 
-    for line in input {
+    for (card_num, line) in input.enumerate() {
         winning_numbers.clear();
+
+        let mut num_current_card = *cards_count.get(&card_num).unwrap_or(&0);
+        num_current_card += 1;
+        cards_count.insert(card_num, num_current_card);
 
         let line = line.unwrap();
         let after_card = line.split_once(':').unwrap().1;
@@ -26,22 +32,23 @@ fn solve(path: &str) -> i32 {
             winning_numbers.insert(w.parse().unwrap());
         }
 
-        let mut points = 0;
+        let mut bonus_card_amount = 0;
         for a in have.split_whitespace() {
             let num: i32 = a.parse().unwrap();
             if winning_numbers.contains(&num) {
-                if points == 0 {
-                    points = 1;
-                } else {
-                    points *= 2;
-                }
+                bonus_card_amount += 1;
             }
         }
 
-        solution += points;
+        for bonus_card in 0..bonus_card_amount {
+            let bonus_card_num = card_num + bonus_card + 1;
+
+            let current_count = *cards_count.get(&bonus_card_num).unwrap_or(&0);
+            cards_count.insert(bonus_card_num, current_count + num_current_card);
+        }
     }
 
-    solution
+    cards_count.values().sum()
 }
 
 fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<std::fs::File>>> {
