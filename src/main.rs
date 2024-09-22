475622b9ff1457d6c07f715375106f341c7aabc1
Katherine Broadbent <kategory.bee@gmail.com>
2024-09-22 19:05:51 +1000

    Day 8 part 2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use regex::Regex;
 fn main() {
     let test_result = solve("test.txt");
     assert_eq!(test_result, 6, "test input failed");
+    println!("Test passed");
 
     let result = solve("input.txt");
     println!("result: {result}");
@@ -13,23 +14,73 @@ fn main() {
 fn solve(path: &str) -> i64 {
     let input = Input::parse(path);
 
-    let mut at = "AAA";
-    let mut step = 0;
+    let starting_nodes: Vec<&str> = input
+        .network
+        .keys()
+        .filter(|n| n.ends_with('A'))
+        .map(|n| n.as_str())
+        .collect();
+    assert!(!starting_nodes.is_empty());
 
-    while at != "ZZZ" {
-        let direction = input.directions[step % input.directions.len()];
-        let choices = &input.network[at];
+    // In this loop we collect the initial node name, how long until we see the node ending with
+    // 'Z'. In our input the distant to the 'Z' node (there's only 1!), is the same as the cycle
+    // length to get back to it. I assume the input was constructed this way. But i dislike that
+    // it wasn't specified in the problem text.
+    //
+    // AND MORE IMPORTANTLY IT ISN'T TRUE FOR THE EXAMPLE DATA. which sucks even more. but the
+    // lowest common multiple will still work on that data set
+    let mut cycles = Vec::new();
 
-        step += 1;
+    for &start in starting_nodes.iter() {
+        let mut step = 0;
+        let mut dir_idx = 0;
 
-        if direction == b'L' {
-            at = &choices.0;
-        } else {
-            at = &choices.1;
+        // list of nodes / states we've already seen.
+        // key is (Node Name, index in 'directions').
+        // value is how many 'steps' in we were when we saw it, so we can figure out the length from
+        // the starting node to the beginning of the cycle.
+        let mut visited: HashMap<(String, usize), i64> = HashMap::new();
+        let mut current_pos = start;
+
+        loop {
+            // insert details for current node. the logic is a bit cleaner for the initial starting
+            // point if we do this up front, then take a step
+            let already_visited = visited.insert((current_pos.to_owned(), dir_idx), step);
+
+            if let Some(steps_last_seen_at) = already_visited {
+                // ...
+                let cycle_length = step - steps_last_seen_at;
+                dbg!(cycle_length);
+                break;
+            }
+
+            step += 1;
+
+            let take_direction = input.directions[dir_idx];
+            dir_idx += 1;
+            if dir_idx >= input.directions.len() {
+                dir_idx = 0;
+            }
+
+            // do the step thing.
+            let go_to = if take_direction == b'L' {
+                input.network[current_pos].0.as_str()
+            } else {
+                input.network[current_pos].1.as_str()
+            };
+            current_pos = go_to;
+
+            if current_pos.ends_with('Z') {
+                println!(
+                    "ending position for {} at {} after {} steps",
+                    start, current_pos, step
+                );
+                cycles.push(step);
+            }
         }
     }
 
-    step as i64
+    cycles.iter().fold(1, |x, y| num::integer::lcm(x, *y))
 }
 
 #[derive(Debug)]
